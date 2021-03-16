f2daf0fec468f096614084d89cfe66dda7aeffc9
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-16 03:13:51 +0100

    ex14 gotov

---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+function convertStrToInt(str){
+    var a = parseInt(str);
+    console.log(a);
+}
+convertStrToInt("44");
+convertStrToInt("27");
+convertStrToInt("JeffBezos")
+module.exports = convertStrToInt;
